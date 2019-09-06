b16cfeef2dd0bad01e854bc499f39beb7938f8e3
Aleh <yem1403@gmail.com>
2019-09-06 21:30:26 +0300

    Small fixes

--- src/DaysOfWeek.java
+++
@@ -1,5 +0,0 @@
-public enum DaysOfWeek {
-
-    MONDAY, TUESDAY, WEDNESDAY, THURTHDAY, FRIDAY, SATURDAY, SUNDAY
-
-}
---
+++ src/by/epam/ayem/module4/DaysOfWeek.java
@@ -0,0 +1,7 @@
+package by.epam.ayem.module4;
+
+public enum DaysOfWeek {
+
+    MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY
+
+}
